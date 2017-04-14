5d5f67bd1d8196e3fc040e4104de5d968a01f444
gonczor <wiktor.gonczaronek@gmail.com>
2017-04-14 18:16:47 +0200

    created client

--- app/src/main/java/com/gonczor/githubsearcher/rest/Client.java
+++ app/src/main/java/com/gonczor/githubsearcher/rest/Client.java
@@ -1,5 +1,21 @@
 package com.gonczor.githubsearcher.rest;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class Client {
+    private static final String GITHUB_API_URL = "https://api.github.com/";
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient(){
+        if (retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(GITHUB_API_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
 }
