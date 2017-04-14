973616f5e951c05f5abbc5269f57c3728da8cbe5
gonczor <wiktor.gonczaronek@gmail.com>
2017-04-14 18:49:25 +0200

    Call for user's repos established

--- app/src/main/java/com/gonczor/githubsearcher/activities/MainActivity.java
+++ app/src/main/java/com/gonczor/githubsearcher/activities/MainActivity.java
@@ -2,8 +2,18 @@ package com.gonczor.githubsearcher.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.gonczor.githubsearcher.R;
+import com.gonczor.githubsearcher.models.Repo;
+import com.gonczor.githubsearcher.rest.ApiInterface;
+import com.gonczor.githubsearcher.rest.Client;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +21,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String user = "gonczor";
+
+        ApiInterface service = Client.getClient().create(ApiInterface.class);
+
+        Call<List<Repo>> repoCall = service.getRepo(user);
+
+        repoCall.enqueue(new Callback<List<Repo>>() {
+            @Override
+            public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
+                Log.d("MainActivity", "SUCCESS!");
+            }
+
+            @Override
+            public void onFailure(Call<List<Repo>> call, Throwable t) {
+                Log.d("MainActivity", t.toString());
+            }
+        });
     }
 }
--- app/src/main/java/com/gonczor/githubsearcher/rest/ApiInterface.java
+++ app/src/main/java/com/gonczor/githubsearcher/rest/ApiInterface.java
@@ -1,5 +1,18 @@
 package com.gonczor.githubsearcher.rest;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+import com.gonczor.githubsearcher.models.Repo;
+import com.gonczor.githubsearcher.models.User;
+
+import java.util.List;
 
 public interface ApiInterface {
+    @GET("users/{user}")
+    Call<User> getUser(@Path("user") String user);
+
+    @GET("users/{user}/repos")
+    Call<List<Repo>> getRepo(@Path("user") String user);
 }
