eea76479292ec887d0d3c5a2d1ba7d35986d77de
gonczor <wiktor.gonczaronek@gmail.com>
2017-05-10 15:31:18 +0200

    Updated requirements

--- app/src/androidTest/java/com/gonczor/githubsearcher/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/gonczor/githubsearcher/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.gonczor.githubsearcher", appContext.getPackageName());
+        assertEquals("com.gonczor.githubsearcher/dupa", appContext.getPackageName());
     }
 }
